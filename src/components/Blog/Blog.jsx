88e9af91229ad09f4cc62b9cbd7bc269d9a492fc
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-21 21:56:50 +0600

    showing blog as cards

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,23 @@
-import PropTypes from 'prop-types';
+import PropTypes, { bool } from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({ blog }) => {
+    const { title, cover, author_img, author, posted_date, reading_time } = blog
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center gap-2'>
+                    <img className='w-10' src={author_img} alt="" />
+                    <div>
+                        <h3>{author}</h3>
+                        <p className='text-gray-500 text-sm'>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <h3>{reading_time} mins</h3>
+                </div>
+            </div>
+            <h1>{title}</h1>
         </div>
     );
 };
