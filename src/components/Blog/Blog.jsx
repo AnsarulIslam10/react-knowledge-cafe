2f3f9a982acb984edea1fd1d60fe97d5ac4b54c8
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-21 22:34:52 +0600

    responsive cards

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,7 +3,7 @@ import PropTypes, { bool } from 'prop-types';
 const Blog = ({ blog }) => {
     const { title, cover, author_img, author, posted_date, reading_time, hashtags } = blog
     return (
-        <div className='pb-10 border-b-2 mt-10'>
+        <div className='pb-10 border-b-2 mt-10 p-4'>
             <img className='w-full mb-8 rounded-xl' src={cover} alt={`cover picture of the title ${title}`} />
             <div className='flex justify-between items-center'>
                 <div className='flex items-center gap-6'>
@@ -18,7 +18,7 @@ const Blog = ({ blog }) => {
                     <i className="fa-regular fa-bookmark text-gray-500"></i>
                 </div>
             </div>
-            <h1 className='text-[40px] font-semibold mt-4 mb-4'>{title}</h1>
+            <h1 className='text-2xl sm:text-3xl md:text-[40px] font-semibold mt-4 mb-4'>{title}</h1>
             <p className='text-gray-500 text-xl'>
                 {
                     hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash} </a></span>)
