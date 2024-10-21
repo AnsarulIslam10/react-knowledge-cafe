b774d25aac7ca30f28ff64fc2ab56d8428680d5d
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-21 23:43:06 +0600

    bookmark section update styling

--- src/Bookmark/Bookmark.jsx
+++ src/Bookmark/Bookmark.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Bookmark = ({bookmark}) => {
     const {title} = bookmark;
     return (
-        <div className="bg-slate-200 m-4 p-4 border rounded-xl shadow">
+        <div className="bg-white mb-4 mt-4 p-4 rounded-xl shadow">
             <h3 className="text-2xl">{title}</h3>
         </div>
     );
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,8 +3,8 @@ import Bookmark from "../../Bookmark/Bookmark";
 const Bookmarks = ({bookmarks}) => {
 
     return (
-        <div className="md:w-1/3">
-            <h2 className="text-3xl">Bookmarked Blogs: {bookmarks.length}</h2>
+        <div className="md:w-1/3 bg-gray-100 ml-4 mt-10 p-4 rounded-xl">
+            <h2 className="text-3xl font-bold">Bookmarked Blogs: {bookmarks.length}</h2>
             {
                 bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
             }
