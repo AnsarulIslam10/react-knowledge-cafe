d18d4dbbdd2632fa9c125940210e85bad6ef801b
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-21 20:00:35 +0600

    header added

--- src/components/Header/Blogs/Blogs.jsx
+++ src/components/Header/Blogs/Blogs.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
+
     return (
         <div>
-                
+            <h2>Blog: {blogs.length}</h2>
         </div>
     );
 };
